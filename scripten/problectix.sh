db7aff8c7ac281bce0b101d75ff377366affdbde
jeffbeck <jeffbeck>
2004-06-05 21:54:50 +0000

    *** empty log message ***

--- scripten/problectix.sh
+++ scripten/problectix.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# creates post-script from a *.tex-file
+# $1:  the directory, where the *.tex-source is located
+# $2:  the name of the *.tex-file (without the extension)
+
 export TEXINPUTS="$HOME/mytex//::$HOME/cvs-workspace/kaelteaufgaben//::./figures"
 
 echo "<p> TEXINPUTS: $TEXINPUTS <p>"
--- scripten/tex2png.sh
+++ scripten/tex2png.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# creates a *.png picture of a *.tex-file
+# $1:  the directory, where the *.tex-source is located
+# $2:  the name of the *.tex-file (without the extension)
+
 export TEXINPUTS="$HOME/mytex//::$HOME/cvs-workspace/kaelteaufgaben//::./figures"
 
 echo "<p> TEXINPUTS: $TEXINPUTS <p>"
@@ -7,4 +11,5 @@ latex temp-datei.tex
 
 dvips -x2074 -y2074 -T 40cm,80cm temp-datei.dvi
 
+# -crop 0x0 # Beschneiden
 convert -crop 0x0 temp-datei.ps png:$2.tex-gen.png
